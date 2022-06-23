e1397279fef2d8db5004080ad615d3688c1475f4
arunbhalli <tomarbhalli@gmail.com>
2022-06-23 19:23:53 +0200

    adds menu for app

---
+++ src/componenets/TopMenu.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const TopMenu = () => {
+  return (
+    <>
+      <header>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-xl-12 col-lg-12'>
+              <nav>
+                <ul>
+                  <li>
+                    <a href='#'>Home</a>
+                  </li>
+                  <li>
+                    <a href='#'>Home</a>
+                  </li>
+                  <li>
+                    <a href='#'>Home</a>
+                  </li>
+                  <li>
+                    <a href='#'>Home</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default TopMenu;
