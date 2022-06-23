f9e2b94e57ea9373dab7b890595d935d43636d53
arunbhalli <tomarbhalli@gmail.com>
2022-06-23 19:47:15 +0200

    adds style in top menu adds diffrent files in sass for header,mixin

--- src/componenets/TopMenu.jsx
+++ src/componenets/TopMenu.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
-
+import '../componenets/_topMenu.scss';
+const Menu = ['Home', 'Men', 'Women', 'Kids', 'Best Seller'];
 const TopMenu = () => {
   return (
     <>
-      <header>
+      <header className='menuHeader'>
         <div className='container-fluid'>
           <div className='row'>
             <div className='col-xl-12 col-lg-12'>
-              <nav>
+              <nav className='menu'>
                 <ul>
-                  <li>
-                    <a href='#'>Home</a>
-                  </li>
-                  <li>
-                    <a href='#'>Home</a>
-                  </li>
-                  <li>
-                    <a href='#'>Home</a>
-                  </li>
-                  <li>
-                    <a href='#'>Home</a>
-                  </li>
+                  {Menu.map((item, index) => (
+                    <li key={index}>
+                      <a href='#'>{item}</a>
+                    </li>
+                  ))}
                 </ul>
               </nav>
             </div>
